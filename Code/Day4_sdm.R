6cdc120e29b5d33eb7bb62aea69cd173e9467e16
Duccio Rocchini <ducciorocchini@gmail.com>
2025-02-19 10:24:27 +0100

    Update Day4_sdm.R

--- Code/Day4_sdm.R
+++ Code/Day4_sdm.R
@@ -64,3 +64,64 @@ p1 <- predict(m1, newdata=preds)
 # plot the output
 plot(p1, col=viridis(100))
 points(species[species$Occurrence == 1,], pch=16)
+
+# colorist
+
+# https://besjournals.onlinelibrary.wiley.com/doi/epdf/10.1111/2041-210X.13477
+
+# First map
+
+library(colorist)
+data("fiespa_occ")
+
+# First map with single distributions
+m1 <- metrics_pull(fiespa_occ)
+p1 <- palette_timecycle(fiespa_occ)
+map_multiples(m1, p1, ncol=4, labels=names(fiespa_occ))
+
+# Common map
+m1_distill <- metrics_distill(fiespa_occ)
+map_single(m1_distill, p1)
+
+# Spheric graph
+legend_timecycle(p1, origin_label="Jan 1")
+
+# Second example single maps
+data("elephant_ud")
+m2 <- metrics_pull(elephant_ud)
+p2 <- palette_set(2)
+map_multiples(m2, p2, ncol = 2, lambda_i=-5,labels=names(elephant_ud))
+
+# Common map
+m2_distill <- metrics_distill(elephant_ud)
+map_single(m2_distill, p2, lambda_i=-5)
+
+# Legends
+legend_set(p2, group_labels=names(elephant_ud))
+
+# cartograms
+
+# https://cran.r-project.org/web/packages/cartogram/readme/README.html
+
+library(cartogram)
+library(sf)
+library(tmap)
+
+data("World")
+afr <- World[World$continent == "Africa", ]
+
+# project the map
+afr <- st_transform(afr, 3395)
+worldn <- st_transform(World, 3395)
+
+# construct cartogram
+afr_cont <- cartogram_cont(afr, "pop_est", itermax = 5)
+worldn_cont <- cartogram_cont(worldn, "pop_est", itermax = 5)
+
+# plot it
+tm_shape(afr_cont) + tm_polygons("pop_est", style = "jenks") +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
+tm_shape(worldn_cont) + tm_polygons("pop_est", style = "jenks") +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
